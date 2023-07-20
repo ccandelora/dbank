fce999635ac01c6be7b2b3a90bf153e03904ce28
Chris Candelora <chris@inigus.com>
2023-07-20 17:26:53 -0400

    moved to github

--- src/dbank_backend/main.mo
+++ src/dbank_backend/main.mo
@@ -1,5 +1,36 @@
-actor {
-  public query func greet(name : Text) : async Text {
-    return "Hello, " # name # "!";
-  };
-};
+import Debug "mo:base/Debug";
+import Time "mo:base/Time";
+import Float "mo:base/Float";
+
+actor DBank {
+  stable var currentValue: Float = 300;
+  stable var startTime = Time.now(); 
+
+  public func deposit(amount: Float){
+      currentValue += amount;
+      Debug.print(debug_show(currentValue));
+   };
+
+  public func withdraw(amount: Float){
+      let tempValue: Float = currentValue - amount;
+      if (tempValue >= 0){
+          currentValue -= amount;
+          Debug.print(debug_show(currentValue));
+      }else {
+         Debug.print("Not enough money");
+      }
+   };
+
+    public query func getBalance(): async Float {
+        return currentValue;
+    };
+
+    public func compound(){
+      let currentTime = Time.now();
+      let timeElapsedNS = currentTime - startTime;
+      let timeElapsedS = timeElapsedNS / 1000000000;
+      let interest = 0.01;
+      currentValue := currentValue * (interest ** Float.fromInt(timeElapsedS));
+      startTime := currentTime;
+    };
+}
--- src/dbank_frontend/src/index.js
+++ src/dbank_frontend/src/index.js
@@ -1,19 +1,34 @@
 import { dbank_backend } from "../../declarations/dbank_backend";
 
+window.addEventListener("load", async () => {
+  getTotalAmount();
+}); 
+
 document.querySelector("form").addEventListener("submit", async (e) => {
   e.preventDefault();
-  const button = e.target.querySelector("button");
+  
+  const button =e.target.querySelector("#submit-btn");
+  button.disabled = true;
+  
+  const depositAmount = parseFloat(document.getElementById("deposit-amount").value);
+  const withdrawAmount = parseFloat(document.getElementById("withdrawal-amount").value);
+  
+  if (depositAmount.length > 0){
+    await dbank_backend.deposit(depositAmount);
+  }
+  if (withdrawAmount.length > 0){
+    await dbank_backend.withdraw(withdrawAmount);
+  }
 
-  const name = document.getElementById("name").value.toString();
-
-  button.setAttribute("disabled", true);
-
-  // Interact with foo actor, calling the greet method
-  const greeting = await dbank_backend.greet(name);
-
-  button.removeAttribute("disabled");
-
-  document.getElementById("greeting").innerText = greeting;
-
-  return false;
+  await dbank_backend.compound();
+  getTotalAmount();
+  button.disabled = false;
+  
+  document.getElementById("deposit-amount").value = "";
+  document.getElementById("withdrawal-amount").value = "";
 });
+
+async function getTotalAmount(){
+  const currentAmount = await dbank_backend.getBalance();
+  document.getElementById("currentAmount").innerHTML = currentAmount.toFixed(2);
+};
